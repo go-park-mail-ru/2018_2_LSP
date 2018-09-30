e9c6195af2024a0eb0b167a2a60a7036ee71d5ed
TheSDTM <yaroslavperf@gmail.com>
2018-09-30 19:49:27 +0300

    Added CORS

--- webserver/handlers.go
+++ webserver/handlers.go
@@ -9,6 +9,7 @@ import (
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	err := checkAuth(r)
 
