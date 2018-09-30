fa2e7efcc3524833c81cb8031215d00c4f9a1443
TheSDTM <yaroslavperf@gmail.com>
2018-09-30 20:36:03 +0300

    Fixed CORS

--- webserver/handlers.go
+++ webserver/handlers.go
@@ -40,6 +40,10 @@ func authHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 
+	if r.Method == http.MethodOptions {
+		return
+	}
+
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		writeJSONToStream(w, apiError{1, "Method Not Allowed"})
