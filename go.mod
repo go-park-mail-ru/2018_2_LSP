7f7c0b8d25790088f49651fcbb209d4bb5d2da34
Moleque <molecada@yandex.ru>
2018-10-05 16:55:36 +0300

    Registration and profile added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module LSP
+
+require github.com/satori/go.uuid v1.2.0
--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/satori/go.uuid"
 )
 
 type User struct {
@@ -15,6 +18,61 @@ type User struct {
 	Score     int    `json:"score"`
 }
 
+var session = make(map[string]string)
+
+var users = map[string]User{
+	"moleque@mail.ru": User{
+		Username:  "Moleque",
+		Email:     "moleque@mail.ru",
+		Password:  "123",
+		Gamecount: 5,
+		Score:     72,
+	},
+	"molecada@yandex.ru": User{
+		Username:  "Molecada",
+		Email:     "molecada@yandex.ru",
+		Password:  "12345",
+		Gamecount: 7,
+		Score:     10,
+	},
+	"l@yandex.ru": User{
+		Username:  "Llll",
+		Email:     "l@yandex.ru",
+		Password:  "12345",
+		Gamecount: 7,
+		Score:     7,
+	},
+	"m@yandex.ru": User{
+		Username:  "Mlll",
+		Email:     "m@yandex.ru",
+		Password:  "12345",
+		Gamecount: 7,
+		Score:     7,
+	},
+	"n@yandex.ru": User{
+		Username:  "Nlll",
+		Email:     "n@yandex.ru",
+		Password:  "12345",
+		Gamecount: 7,
+		Score:     7,
+	},
+	"c@yandex.ru": User{
+		Username:  "Clll",
+		Email:     "c@yandex.ru",
+		Password:  "12345",
+		Gamecount: 7,
+		Score:     7,
+	},
+	"v@yandex.ru": User{
+		Username:  "Vlll",
+		Email:     "v@yandex.ru",
+		Password:  "12345",
+		Gamecount: 7,
+		Score:     7,
+	},
+}
+
+//========
 func cors(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -25,14 +83,19 @@ func cors(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 }
 
-func leaderboards(w http.ResponseWriter, r *http.Request, users *[]User) {
+func leaderboards(w http.ResponseWriter, r *http.Request) {
 	pageNumer := r.URL.Query().Get("page")
 	if pageNumer == "" {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("no have correct params"))
 		return
 	} else {
-		jsonUsers, err := json.Marshal(&users)
+		sliceUser := make([]User, 0, 10)
+		for _, user := range users {
+			sliceUser = append(sliceUser, user)
+		}
+
+		jsonUsers, err := json.Marshal(&sliceUser)
 		if err != nil {
 			log.Printf("cannot marshal:%s", err)
 		}
@@ -41,23 +104,32 @@ func leaderboards(w http.ResponseWriter, r *http.Request, users *[]User) {
 }
 
 func profile(w http.ResponseWriter, r *http.Request) {
-	session, err := r.Cookie("session")
-	loggedIn := (err != http.ErrNoCookie)
-
-	if loggedIn {
-		w.Write([]byte("Goooood!" + session.Value))
+	cookie, err := r.Cookie("session")
+	if err == http.ErrNoCookie {
+		log.Println("coook")
+		w.WriteHeader(http.StatusUnauthorized)
+		return
 	}
+
+	email, ok := session[cookie.Value]
+	if !ok {
+		log.Println("ses")
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	user := users[email]
+	response, err := json.Marshal(user)
+	if err != nil {
+		log.Println("cannot marshal user:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(response)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	expiration := time.Now().Add(24 * time.Hour)
-	cookie := http.Cookie{
-		Name:    "session",
-		Value:   "moleque",
-		Expires: expiration,
-	}
-	http.SetCookie(w, &cookie)
-	w.Write([]byte("Good!"))
+
 }
 
 func logout(w http.ResponseWriter, r *http.Request) {
@@ -70,59 +142,43 @@ func logout(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func main() {
-	users := []User{
-		{
-			Username:  "Moleque",
-			Email:     "moleque@mail.ru",
-			Password:  "123",
-			Gamecount: 5,
-			Score:     72,
-		},
-		{
-			Username:  "Molecada",
-			Email:     "molecada@yandex.ru",
-			Password:  "12345",
-			Gamecount: 7,
-			Score:     10,
-		},
-		{
-			Username:  "Llll",
-			Email:     "l@yandex.ru",
-			Password:  "12345",
-			Gamecount: 7,
-			Score:     7,
-		},
-		{
-			Username:  "Mlll",
-			Email:     "m@yandex.ru",
-			Password:  "12345",
-			Gamecount: 7,
-			Score:     7,
-		},
-		{
-			Username:  "Nlll",
-			Email:     "n@yandex.ru",
-			Password:  "12345",
-			Gamecount: 7,
-			Score:     7,
-		},
-		{
-			Username:  "Clll",
-			Email:     "c@yandex.ru",
-			Password:  "12345",
-			Gamecount: 7,
-			Score:     7,
-		},
-		{
-			Username:  "Vlll",
-			Email:     "v@yandex.ru",
-			Password:  "12345",
-			Gamecount: 7,
-			Score:     7,
-		},
+func signup(w http.ResponseWriter, r *http.Request) {
+	request := &User{}
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(request)
+	if err != nil {
+		log.Printf("err %s", request)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
+	if _, exists := users[request.Email]; request.Email == "" || exists == true {
+		log.Printf("user %s is exist", request.Email)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	users[request.Email] = User{
+		Username:  request.Username,
+		Email:     request.Email,
+		Password:  request.Password,
+		Gamecount: 0,
+		Score:     0,
+	}
+	sessionId := uuid.NewV4().String()
+	session[sessionId] = request.Email
+
+	expiration := time.Now().Add(24 * time.Hour)
+	cookie := http.Cookie{
+		Name:    "session",
+		Value:   sessionId,
+		Expires: expiration,
+	}
+	http.SetCookie(w, &cookie)
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
 	http.HandleFunc("/session", func(w http.ResponseWriter, r *http.Request) {
 		cors(w, r)
 		switch r.Method {
@@ -142,9 +198,20 @@ func main() {
 		case http.MethodGet:
 			page := r.URL.Query().Get("page")
 			if page != "" {
-				leaderboards(w, r, &users)
+				leaderboards(w, r)
+			} else {
+				profile(w, r)
 			}
+		case http.MethodPost:
+			signup(w, r)
 		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err := fmt.Sprintf("cannot get %s", r.URL)
+		log.Printf(err)
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(err))
 	})
 
 	log.Printf("Try to start http server http://127.0.0.1:8080")
